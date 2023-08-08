dad7009f0d0ebf1c87f7c166bd8bb2dc5d8f35b5
Gabriel LR <dayta.scientist@gmail.com>
2023-08-08 09:14:10 -0300

    Show a picture of a cat with the first word

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import './App.css'
 import { useGetRandomFacts } from './hooks/useGetRandomFacts'
+import { useCatImage } from './hooks/useCatImage'
 
 
 function App() {
  const {fact, fetchRandom } = useGetRandomFacts()
+ const { image } = useCatImage({ fact })
   return (
     <>
       <h1>Kitten App</h1>
       {fact && <p>{fact}</p>}
+      {image && <img src={image} alt={`Image extracted using the first word for ${fact}`} />}
     </> 
   )
 }
---
+++ src/hooks/useCatImage.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+
+const CAT_PREFIX_IMAGE_URL = "https://cataas.com";
+
+export const useCatImage = ({ fact }) => {
+  const [image, setImage] = useState()
+
+  const fetchImage = async () => {
+    const firstWord = fact.split(' ', 1).join(' ')
+    console.log(firstWord)
+    try {
+      const response = await fetch(`https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`)
+      if(!response.ok) {
+        throw new Error("Network response was not ok")
+      }
+      const { url } = await response.json()
+      setImage(url)
+
+    } catch (error) {
+      console.error('There was a problem with the fetch operation:', error)
+    }
+    
+    }
+    useEffect(() => {
+      fetchImage()
+    }, [fact])
+
+    return { image: `${CAT_PREFIX_IMAGE_URL}${image}` }
+}
+
+  
+
+  
+  
+
+  
