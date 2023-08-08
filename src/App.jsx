ab73e78b1cdc83f4b3644d8609db5b22216cec1e
Gabriel LR <dayta.scientist@gmail.com>
2023-08-08 09:31:03 -0300

    Add button: Get new fact

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,17 @@ import { useCatImage } from './hooks/useCatImage'
 function App() {
  const {fact, fetchRandom } = useGetRandomFacts()
  const { image } = useCatImage({ fact })
+
+ const handleClick = async () => {
+  fetchRandom()
+}
+
   return (
     <>
       <h1>Kitten App</h1>
+
+      <button onClick={handleClick}>Get new fact</button>
+      
       {fact && <p>{fact}</p>}
       {image && <img src={image} alt={`Image extracted using the first word for ${fact}`} />}
     </> 
